511122d1fc0e173c12bbcec586b7c15593039729
d <79714611+ndmarroquin@users.noreply.github.com>
2022-05-14 20:05:45 -0600

    Add files via upload

---
+++ js/order.js
@@ -0,0 +1,12 @@
+let orden = [
+
+    //order_id	total_item	shipping_fee	tax 	total_cost	order_date	delivery_date	ship_name 	ship_address 	tracking_number	delivery_status
+
+    [1000, 4, 7,  0.0925,   50.02,	"17/10/2018",   "20/10/2018",	"Anna Addison",	    "1325 Candy Rd, San Francisco, CA 96123",	"ZW60001",      1],
+    [1001, 5, 8,  0.06,	    62.45,  "15/10/2018",   "18/10/2018",   "Carol Campbell",   "1931 Brown St, Gainesville, FL 85321",	    "AB61001",      0],
+    [1002, 7, 10, 0.087,	40.33,	"14/10/2018",	"17/10/2018",	"Julia Jones",      "1622 Seaside St, Seattle, WA 32569",   	"CD62001", 	    1],
+    [1003, 9, 20, 0.0625,	70.98,	"12/10/2018",	"15/10/2018",	"Irene Everly", 	"1756 East Dr, Houston, TX 28562",	        "KB63001", 	    0],
+    [1004, 6, 7,  0.0625,	30.45,	"16/10/2018",	"19/10/2018",	"Rachel Rose",	    "1465 River Dr, Boston, MA 43625",  	    "IK64001",   	1],
+    [1005, 5, 8,  0.0625,	100.2,	"13/10/2018",	"16/10/2018",	"Sophie Sutton",	"1896 West Dr, Portland, OR 65842",	        "OP65001",   	0],
+    [1006, 3, 5,  0.1025,	58.52,	"21/10/2018",	"24/10/2018",	"Wendy West",	    "1252 Vine St, Chicago, IL 73215",  	    "XH66001",  	1],
+]
