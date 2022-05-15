d89c53d26e772a5eff0c09265d1ea21d9f2c3b95
d <79714611+ndmarroquin@users.noreply.github.com>
2022-05-14 20:04:24 -0600

    Add files via upload

---
+++ js/category.js
@@ -0,0 +1,12 @@
+let category = [
+
+    //category_id 	category_name
+
+    [4000,	"Laptops"],
+    [4100,	"Mobile phones"],
+    [4200,	"Tablets"],
+    [4300,	"Servers"],
+    [4400,	"PC"],
+    [4500,	"Cables"],
+    [14600,	"Headphones"],
+]
